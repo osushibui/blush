515cfe7da5f6ab087180aa23e95d4028e92de719
Alfie <torcixld@gmail.com>
2020-02-23 16:18:50 +0000

    Update kickUser.js

--- cmd/kickUser.js
+++ cmd/kickUser.js
@@ -1,3 +1,9 @@
+const config = require("../config.json");
+const fs = require('fs');
+
+module.exports = function(client) {
+    client.on('message', async message => {
+        
 if(command === "kick") {
     if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
@@ -16,3 +22,6 @@ if(command === "kick") {
     message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
   }
+        
+    })
+}
