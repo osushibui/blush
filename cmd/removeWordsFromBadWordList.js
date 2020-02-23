4101f7dec57be2f4563553539f430b4508e55b98
Alfie <torcixld@gmail.com>
2020-02-23 15:40:06 +0000

    Update removeWordsFromBadWordList.js

--- cmd/removeWordsFromBadWordList.js
+++ cmd/removeWordsFromBadWordList.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports = function(client) {
     client.on('message', async message => {
         if (message.content.startsWith(config.bot.prefix + "nazi remove")) {
-            if (message.member.id != config.bot.owner_id) { return message.channel.send("This is reserved for ilyt, sorry!") }
+            if (message.member.id != config.bot.owner_id) { return message.channel.send("Only Community Managers can use this command.") }
             // Please make a PR and help me fix this
             var msg = message.content;
             msg = msg.split(config.bot.prefix + "nazi remove ");
