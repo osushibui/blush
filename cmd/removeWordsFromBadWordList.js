de5a603f58831fe7f3fa4870f7c0bd46f1e8c1ae
Alfie <torcixld@gmail.com>
2020-02-23 15:41:47 +0000

    Changed command name because nazi is not appropriate :sunglasses:

--- cmd/removeWordsFromBadWordList.js
+++ cmd/removeWordsFromBadWordList.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 
 module.exports = function(client) {
     client.on('message', async message => {
-        if (message.content.startsWith(config.bot.prefix + "nazi remove")) {
+        if (message.content.startsWith(config.bot.prefix + "wordlist remove")) {
             if (message.member.id != config.bot.owner_id) { return message.channel.send("Only Community Managers can use this command.") }
             // Please make a PR and help me fix this
             var msg = message.content;
-            msg = msg.split(config.bot.prefix + "nazi remove ");
+            msg = msg.split(config.bot.prefix + "wordlist remove ");
             console.log(msg)
             msg = msg[1];
             global.naughtyWords = global.naughtyWords.filter(e => e !== msg);
@@ -16,7 +16,7 @@ module.exports = function(client) {
             fs.writeFile('config.json', JSON.stringify(config, null, 2), function(err) {
                 if (err) throw err;
             })
-            return message.channel.send(`Removed ${msg} from blacklist!`)
+            return message.channel.send(`Removed ${msg} from blacklist.`)
         }
     })
 }
