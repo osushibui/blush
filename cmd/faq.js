5cb47aa973c839d4e83c68a63d455dc4c469177f
James <51536154+Millenialss@users.noreply.github.com>
2020-02-23 16:02:40 +0000

    Update with relevant info

--- cmd/faq.js
+++ cmd/faq.js
@@ -28,26 +28,30 @@ module.exports = function (client) {
                 .addField("**Description**", "Depending on the situation there's many ways to contact the Shibui staff (discord, in-game). The most reliable way of getting in contact with staff is to message a Community Manager (or Owner) in-game or on discord if the issue is urgent or not about restrictions.")
             } else if (msg.toLowerCase() == "cert") {
                 embed.setTitle("'Connection failed. Retrying in 30s'")
-                .addField("**Description**", "This usually means that the certificate was not installed correctly. Check the pinned messages in #general, download the cert.crt file and click install certificate, set the store location to Local Machine, Place all certificates in the following store, click browse and choose Trusted Root Certification Authorities. Then next and finish. All you need to do after that is restart osu! and then log in.")
+                .addField("**Description**", "This usually means that the certificate was not installed correctly. Download the shibui.crt file (from https://shibui.pw/static/shibui.crt) and click install certificate, set the store location to Local Machine, Place all certificates in the following store, click browse and choose Trusted Root Certification Authorities. Then next and finish. All you need to do after that is restart osu! and then log in.")
             } else if (msg.toLowerCase() == "hosts") {
                 embed.setTitle("Hosts File Error")
-                .addField("**Description**", "If the switcher says it is in read only, or you are still connected to bancho (or another server), open C:\Windows\System32\drivers\etc\hosts with notepad (running as admin) and go to https://ip.yozora.pw/hosts.txt and copy paste the text that you get from there into the hosts file and save it.")
-            } else if (msg.toLowerCase() == "linux/mac") {
+                .addField("**Description**", "If the switcher says it is in read only, or you are still connected to bancho (or another server), open C:\Windows\System32\drivers\etc\hosts with notepad (running as admin) and go to https://shibui.pw/static/hosts.txt and copy paste the text that you get from there into the hosts file and save it.")
+            } else if (msg.toLowerCase() == "mac") {
                 embed.setTitle("Hosts File Error")
-                .addField("**Description**", "If the switcher says it is in read only, or you are still connected to bancho (or another server), open /etc/hosts or /private/etc/hosts with your text editor of choice (running as sudo/admin) and go to https://ip.yozora.pw/hosts.txt and copy paste the text that you get from there into the hosts file and save it then install the certificate that is pinned within #help")
+                .addField("**Description**", "Open /private/etc/hosts with your text editor of choice (running as sudo/admin) and go to https://shibui.pw/static/hosts.txt and copy paste the text that you get from there into the hosts file and save it then install the certificate that can be downloaded from https://shibui.pw/static/shibui.crt")
+            } else if (msg.toLowerCase() == "linux") {
+                embed.setTitle("Hosts File Error")
+               .addField("**Description**", "Open /etc/hosts with your text editor of choice (running as sudo/admin) and go to https://shibui.pw/static/hostslinux.txt and copy paste the text that you get from there into the hosts file and save it then install the certificate that can be downloaded from https://shibui.pw/static/shibui.crt. For help installing the certificate refer to: https://shibui.pw/doc/linux")
             } else if (msg.toLowerCase() == "help") {
                 embed.setTitle("FAQ Help")
-                .setDescription("Do !faq (field title) to use it.")
+                .setDescription("Do faq (field title) to use it.")
                 .addField("Email", "Explains why you haven't recieved the email verification :facepalm:")
                 .addField("Appeals", "Explains how appeals works within Shibui")
                 .addField("Contact", "Lists ways to contact the Shibui staff")
                 .addField("Cert", "How to install the certificate manually")
                 .addField("Hosts", "How to edit hosts file manually")
-                .addField("linux/mac", "How to connect on linux or mac")
+                .addField("mac", "How to connect on mac")
+                .addField("linux", "How to connect on linux")
                 .addField("help", "Shows this")
 
             } else {
-                return message.channel.send("This is not a valid option, maybe try >faq help?") 
+                return message.channel.send("This is not a valid option, maybe try faq help?") 
             }
             return message.channel.send(embed)
         }
