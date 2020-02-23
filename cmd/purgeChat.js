a5ab96b414ea06109f3ae81c3f4b504926ecbd20
Alfie <torcixld@gmail.com>
2020-02-23 16:25:09 +0000

    Create purgeChat.js

---
+++ cmd/purgeChat.js
@@ -0,0 +1,20 @@
+const config = require("../config.json");
+const fs = require('fs');
+const Discord = require("discord.js");
+
+module.exports = function(client) {
+    client.on('message', async message => {
+        
+if(command === "purge") {
+        const deleteCount = parseInt(args[0], 10);
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+        
+    })
+}
